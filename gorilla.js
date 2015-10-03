231f8358dbd41d3dccfe234efd14354abb402fac
Samantha Welch <Samantha@Samanthas-MacBook-Pro-2.local>
2015-10-03 11:20:15 -0400

    updating gorilla

--- gorilla.js
+++ gorilla.js
@@ -1,40 +1,37 @@
-var Gorilla = function(game, key){
-	Phaser.Sprite.call(this, game, 10, 0, key);
-	this.anchor.set(0.5);
-	this.checkWorldBounds = true;
-	this.outOfBoundsKill = true;
-	this.exists = true;
-	this.tracking = true;
-	this. scaleSpeed = 0.5;
-};
-var gorillaRock = function(game, key){
-	Phaser.Sprite.call(this, game, 0, 0, key);
-	this.anchor.set(0.5)
-}
 Gorilla.prototype = Object.create(Phaser.Sprite.prototype);
+
 Gorilla.prototype.constructor = Gorilla;
 
-var mX = game.input.player.x;
-var mY = game.input.player.y;
-Gorilla.angle = Math.atan2(Gorilla.position.x - mX, Gorilla.position.y - mY)  * -57.2957795;
+Gorilla.prototype.force = {x:0.0, y:0.0}; 
 
-Gorilla.prototype.throw = function (x, y, angle, speed, gx, gy) {
-    gx = gx || 0;
-    gy = gy || 0;
-    this.reset(x, y);
-    this.scale.set(1);
+function Gorilla(game, x, y) {
+    var gorila = Phaser.Sprite.call(this, game, x, y, 'gorilla');
+    this.scale.set(1.0, 1.0);
+    this.anchor.setTo(0.5, 0.5);
+    game.physics.enable(this, Phaser.Physics.ARCADE);
+    this.body.allowRotation = false;
+    game.add.existing(this);
 
-    this.game.physics.arcade.velocityFromAngle(angle, speed, this.body.velocity);
-    this.angle = angle;
-    this.body.gravity.set(gx, gy);
-};
 
-game.physics.arcade.overlap(player, gorillaRock, hurtHealth, null, this);
-
-Gorilla.prototype.hurtHealth = function(){
-	player.health =- 5;
 }
+Gorilla.prototype.update = function() {
 
-Gorilla.prototype.gorillaHit = function(){
-	gorilla.health =- 1;
+    var mX = game.input.mousePointer.x;
+    var mY = game.input.mousePointer.y;
+    // /* look at the mouse 
+    this.angle = Math.atan2(this.position.x - mX, this.position.y - mY)  * -57.2957795;
+
+    if (wasd.up.isDown) {
+        this.y -= 3;
+    }
+    if (wasd.down.isDown) {
+        this.y += 3;
+    }
+    if (wasd.left.isDown) {
+        this.x -= 3;
+    }
+    if (wasd.right.isDown) {
+        this.x += 3;
+    } 
+
 }
