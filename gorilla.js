f8e5c1b97bb349e7b1a18f4d87e899bd580e4c05
Samantha Welch <Samantha@resnet-130-64-145-207.medford.tufts.edu>
2015-10-03 19:08:06 -0400

    Gorilla not working

--- gorilla.js
+++ gorilla.js
@@ -1,3 +1,16 @@
+var Rock = function (game, key) {
+    Phaser.Sprite.call(this, game, 0, 0, key);
+    this.texture.baseTexture.scaleMode = PIXI.scaleModes.NEAREST;
+    this.anchor.set(0.5);
+    this.checkWorldBounds = true;
+    this.outOfBoundsKill = true;
+    this.exists = false;
+    this.tracking = false;
+    this.scaleSpeed = 0;
+};
+
+Rock.prototype = Object.create(Phaser.Sprite.prototype);
+Rock.prototype.constructor = Rock;
 Gorilla.prototype = Object.create(Phaser.Sprite.prototype);
 
 Gorilla.prototype.constructor = Gorilla;
@@ -5,33 +18,42 @@ Gorilla.prototype.constructor = Gorilla;
 Gorilla.prototype.force = {x:0.0, y:0.0}; 
 
 function Gorilla(game, x, y) {
-    var gorila = Phaser.Sprite.call(this, game, x, y, 'gorilla');
+    var gorilla = Phaser.Sprite.call(this, game, x, y, 'gorilla');
     this.scale.set(1.0, 1.0);
     this.anchor.setTo(0.5, 0.5);
     game.physics.enable(this, Phaser.Physics.ARCADE);
     this.body.allowRotation = false;
     game.add.existing(this);
-
-
 }
+
+Gorilla.prototype.throw = function (x,y) {
+
+        x = x + 10;
+        y = y + 50;
+        Rock.throw(x, y, this.angle - 90, 150, 0, 0);
+
+};
 Gorilla.prototype.update = function() {
 
-    var mX = game.input.mousePointer.x;
-    var mY = game.input.mousePointer.y;
-    // /* look at the mouse 
-    this.angle = Math.atan2(this.position.x - mX, this.position.y - mY)  * -57.2957795;
+    this.throw(this.position.x,this.position.y);
 
-    if (wasd.up.isDown) {
-        this.y -= 3;
-    }
-    if (wasd.down.isDown) {
-        this.y += 3;
-    }
-    if (wasd.left.isDown) {
-        this.x -= 3;
-    }
-    if (wasd.right.isDown) {
-        this.x += 3;
-    } 
 
 }
+Rock.prototype.update = function () {
+    if (this.tracking) {
+        this.rotation = Math.atan2(this.body.velocity.y, this.body.velocity.x);
+    }
+    if (this.scaleSpeed > 0) {
+        this.scale.x += this.scaleSpeed;
+        this.scale.y += this.scaleSpeed;
+    }
+};
+Rock.prototype.throw = function (x, y, angle, speed, gx, gy) {
+    gx = gx || 0;
+    gy = gy || 0;
+    this.reset(x, y);
+    this.scale.set(1);
+    this.game.physics.arcade.velocityFromAngle(angle, speed, this.body.velocity);
+    this.angle = angle;
+    this.body.gravity.set(gx, gy);
+};
