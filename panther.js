25c8d51c00a2677db728222e35be875fbf1e9f3f
Brian P. O'Keefe <brian.o_keefe@tufts.edu>
2015-10-07 21:08:19 -0400

    created 2 panthers

--- panther.js
+++ panther.js
@@ -5,24 +5,38 @@ Panther.prototype.constructor = Panther;
 Panther.prototype.force = {x:0.0, y:0.0}; 
 
 
-function Panther(group, x, y) {
-    var panther = group.create(0, 0, 'panther');
+function Panther(group, x, y, sprite) {
+    var panther = group.create(0, 0, 'panther', null);
     panther.position.x = x;
     panther.position.y = y;
-    panther.scale.setTo(0.05, 0.05);
+    panther.scale.setTo(0.5, 0.5);
     panther.anchor.setTo(0.5, 0.5);
-
+    
     game.physics.enable(panther, Phaser.Physics.ARCADE);
+    var angle;
+    var speed = 100;
+    var player = sprite;
+    console.log("created");
+    return panther;
 }
 
-Panther.prototype.chase = function(player) {
-    var xgoal = player.position.x;
-    var ygoal = player.position.y;
-    var goal_angle = Math.angleBetween(position.x, position.y, player.position.x, player.position.y);
-    caught = false;
-    while (false /*panther is onscreen)*/) {
-	if (!caught) {
-	    
-	}
-    }
+Panther.prototype.create = function() {
+    console.log("ran create");
+    this.set_angle();
+}
+
+Panther.prototype.set_angle = function() {
+    console.log(this);
+    angle = Math.angleBetween(this.position.x, this.position.y,
+			      player.position.x, player.position.y);
+}
+
+Panther.prototype.chase = function() {
+    this.body.velocity.x = speed * Math.sin(angle);
+    this.body.velocity.y = speed * Math.cos(angle);
+}
+
+Panther.prototype.update = function() {
+    console.log("running update");
+    this.chase();
 }
